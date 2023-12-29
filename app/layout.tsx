5bfa14ece9ab39ccf021c8d59f18443356c315c8
Gopal N D <gopalhelpyou@gmail.com>
2023-12-29 10:58:57 +0530

    added assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,48 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
+import {Inter,Space_Grotesk} from "next/font/google"
+
+
+
+import type {Metadata} from 'next'
  
+export const metadata:Metadata = {
+  title : "DevOverflow",
+  description:"DevOverflow",
+  icons:{
+    icon:"/favicon.ico"
+  }
+}
+
+const inter = Inter({
+  subsets:['latin'],
+  weight:['100','200','300','400','500','600','700','800','900'],
+  variable:'--font-inter'
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets:['latin'],
+  weight:['300','400','500','600','700'],
+  variable:'--font-spaceGrotesk'
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    appearance={{
+      elements:{
+        formButtonPrimary:'primary-gradient',
+        footerActionLink:'primary-text-gradient hover:text-primary-500'
+      }
+    }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}
+        >{children}</body>
       </html>
     </ClerkProvider>
   )
